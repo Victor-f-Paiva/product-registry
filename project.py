8b8a3d0b1fcf78c3ed163678bea2763496cacb87
Victor Paiva <victor_eduardof@hotmail.com>
2026-06-28 12:19:55 -0300

    feat(write_int_txt): add function that write on products.txt the products that were registered in the program

--- project.py
+++ project.py
@@ -1,16 +1,34 @@
 import tkinter as tk
 from tkinter import ttk
+import os
 
 # save list
 save_list = []
 def save(name_entry:tk.Entry, type_un_entry:tk.Entry, qtt_entry:tk.Entry, price_entry:tk.Entry):
-    code = f'{len(save_list)+1:04d}'
+    # verifying how many itens the .txt has
+    lines = 1
+    with open('products.txt', 'r') as file:
+        for line in file:
+            if line.strip():
+                lines += 1
+
+    code = f'{lines:04d}'
     product = name_entry.get()
     type_u = type_un_entry.get()
     qtt = qtt_entry.get()
     price = price_entry.get()
     save_list.append((code, product, type_u, qtt, price)
     )
+    write_in_txt('products.txt')
+
+def write_in_txt(existing_txt):
+    if (os.path.exists(existing_txt)):
+        with open(existing_txt, 'a') as file:
+            for code, product, type_unity, qtt, price in save_list:
+                file.write(f"{code}, {product}, {type_unity}, {qtt}, {price}\n")
+                return "Save successfully"
+    else:
+        return "File not found"
 
 
 def open_registry():
