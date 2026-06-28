55dcb2ab691d3b5b7966c19d7152ca71761f014d
Victor Paiva <victor_eduardof@hotmail.com>
2026-06-28 12:21:50 -0300

    test(write_in_txt): test of the function with mocked list and a mocked .txt file. Were tested success and error situations

--- test_project.py
+++ test_project.py
@@ -1,9 +1,12 @@
 import pytest
-from unittest.mock import MagicMock
-from project import save, save_list, open_registry
+from unittest.mock import MagicMock, mock_open, patch, call
+from project import save, save_list, open_registry, write_in_txt
 
 
 def test_save():
+        #cleaning the test list
+        save_list.clear()
+
         #mocking the tk.entrys
         mock_name = MagicMock()
         mock_name.get.return_value = "notebook"
@@ -17,13 +20,15 @@ def test_save():
         mock_price = MagicMock()
         mock_price.get.return_value = "23.23"
 
-        # calling the fundtion save
-        save(
-            name_entry=mock_name, 
-            type_un_entry=mock_type, 
-            qtt_entry=mock_qtt, 
-            price_entry=mock_price
-            )
+        # mocking a existent file. using project.open and project.os, because the test is writing in 'products.txt'
+        with patch('project.open', mock_open(read_data='')), patch('project.os.path.exists', return_value= True):
+            # calling the function save
+            save(
+                name_entry=mock_name, 
+                type_un_entry=mock_type, 
+                qtt_entry=mock_qtt, 
+                price_entry=mock_price
+                )
 
         #checking the list and validating
         assert len(save_list) == 1
@@ -34,3 +39,21 @@ def test_save():
         assert qtt == "1"
         assert price == "23.23"
 
+
+def test_write_int_txt_success():
+    # mocked list
+    mock_save_list = [
+        (1, "notebook", "pcs", 10, 1255.50), 
+        (2, "cigars", "pack", 5, 7.50)
+    ]
+
+    # open file and substituing the global variable
+    with patch('builtins.open', mock_open()) as mocked_file, patch('project.save_list', mock_save_list), patch('os.path.exists', return_value=True):
+        result = write_in_txt('test_productc.txt')
+        assert result == "Save successfully"
+
+
+def test_write_int_txt_error():
+    with patch("builtins.open", side_efect= FileNotFoundError):
+        result = write_in_txt('not_existing_file.txt')
+        assert result == "File not found"
